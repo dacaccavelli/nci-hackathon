2dcdaef99a6ce6a6f6ee62da2b87d50db0f8be01
kylan96 <76014879+kylan96@users.noreply.github.com>
2021-01-28 15:02:00 -0500

    Add files via upload

--- priceAdjust2.py
+++ priceAdjust2.py
@@ -33,8 +33,10 @@ if car == 'black': #black car discount
         print('So 25% off of that price is...')
         print('And a $500 cash bonus!!!')
         print('So now your price is $', cashbonus(discountCalc(bprice, .25), 500))
+        baseprice = cashbonus(discountCalc(bprice, .25), 500)
     else: 
         print('Alright so your total is still at $', discountCalc(20000, .25))
+        baseprice = discountCalc(20000, .25)
 elif car == 'white': # if car is white cash bonus
     wprice= cashbonus(20000, 400)
     vet = input('Are you a veteran? Please type Y/N >> ')
@@ -43,7 +45,10 @@ elif car == 'white': # if car is white cash bonus
         print('So 25% off of that price is...')
         print('And a $500 cash bonus!!!')
         print('So now your price is $', cashbonus(cashbonus(discountCalc(20000, .25), 500), 400))
-    else: print('No worries, with the cash bonus for white cars your price is $', cashbonus(20000, 400))
+        baseprice = cashbonus(cashbonus(discountCalc(20000, .25), 500), 400)
+    else: 
+        print('No worries, with the cash bonus for white cars your price is $', cashbonus(20000, 400))
+        baseprice = cashbonus(20000, 400)
 else:
     print('Alright the color is not white or black so unfortunately, no cash bonus')
     vet = input('Are you a veteran? Please type Y/N >> ')
@@ -53,5 +58,7 @@ else:
         print('$', discountCalc(20000, .25)) # discount if veteran
         print('And a $500 cash bonus!!!')
         print('So now your price is $', cashbonus(discountCalc(20000, .25), 500))
+        baseprice = cashbonus(discountCalc(20000, .25), 500)
     else:
         print('No worries, we still offer great prices! Your price is still at $20000.')
+        baseprice = 20000
