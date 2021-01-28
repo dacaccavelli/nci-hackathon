08c83249f7420dfeb528ef3bb8514f6642fa8624
Daniel Caccavelli <42353038+dacaccavelli@users.noreply.github.com>
2021-01-28 12:16:33 -0500

    Update Price.py

--- Price.py
+++ Price.py
@@ -1,20 +1,24 @@
-import Price
-... Price ('20000', 'MSRP')
-MSRP 20000
+#import Price
+#... Price ('20000', 'MSRP')
+#MSRP 20000
 
-Price('15000', 'Black')
-Black 15000
+#Price('15000', 'Black')
+#Black 15000
 
-Price('19600', 'White')
-White 19600
+#Price('19600', 'White')
+#White 19600
 
-Price('14500', 'Veteran')
-Veteran 14500
+#Price('14500', 'Veteran')
+#Veteran 14500
 
-def multiply(n1, n2):
-    multiply = n1 + n2
-    return multiply
+def discountCalc(n1, n2):
+    discount = n1 * n2
+    return discount
 
-print (multiply(20000, .25))
-print (multiply(20000, .02))
-print (multiply(20000, .725))
+# if car is black
+print(discountCalc(20000, .25)) # discount if black
+
+# if customer is veteran
+print(discountCalc(20000, .25)) # discount if veteran
+
+#print (discountCalc(20000, .725))
